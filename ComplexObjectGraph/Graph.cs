eca60c95619859d8bcd600fe465d81b668cc0707
James Dunlop <james.dunlop@outlook.com>
2015-07-26 13:42:03 +1000

    Initial commit to start on object graph example

---
+++ ComplexObjectGraph/Graph.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ComplexObjectGraph
+{
+    public class Policy
+    {
+        IList<Traveller> Travellers { get; set; }
+
+        IList<Dependent> Dependents { get; set; }
+    }
+
+    public class Traveller
+    {
+    }
+
+    public class Dependent
+    {
+    }
+
+}
