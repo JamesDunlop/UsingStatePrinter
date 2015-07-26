b2b7fbd83e7b787a287da4e984f624d145b40ada
James Dunlop <james.dunlop@outlook.com>
2015-07-26 18:14:04 +1000

    Testing simple print object

--- ComplexObjectGraph/Graph.cs
+++ ComplexObjectGraph/Graph.cs
@@ -1,20 +1,56 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ComplexObjectGraph
 {
     public class Policy
     {
-        IList<Traveller> Travellers { get; set; }
+        public long PolicyId { get; set; }
 
-        IList<Dependent> Dependents { get; set; }
+        public string PolicyNumber { get; set; }
+
+        public DateTime DepartureDate { get; set; }
+
+        public DateTime ReturnDate { get; set; }
+
+        public Country CountryOfResidence { get; set; }
+
+        public IList<Traveller> Travellers { get; set; }
+
+        public IList<Dependent> Dependents { get; set; }
+    }
+
+    public class Country
+    {
+        public long CountryId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
     }
 
     public class Traveller
     {
+        public long TravellerId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
     }
 
     public class Dependent
     {
+        public long DependentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
     }
 
 }
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using ComplexObjectGraph;
+using StatePrinter;
 
 namespace Runner
 {
     class Program
     {
+        static readonly Stateprinter printer = new Stateprinter();
+
         static void Main(string[] args)
         {
+            var policy = new Policy
+            {
+                PolicyId = 4,
+                CountryOfResidence = new Country
+                {
+                    CountryId = 16,
+                    Name = "UK",
+                    Code = "GBR"
+                },
+                PolicyNumber = "QA1200798",
+                DepartureDate = new DateTime(2014, 11, 4),
+                ReturnDate = new DateTime(2014, 11, 12),
+                Travellers = new []
+                {
+                    new Traveller
+                    {
+                        TravellerId = 37,
+                        FirstName = "John",
+                        LastName = "Riley",
+                        DateOfBirth = new DateTime(1984, 2, 5)
+                    },
+                    new Traveller
+                    {
+                        TravellerId = 38,
+                        FirstName = "Julie",
+                        LastName = "Riley",
+                        DateOfBirth = new DateTime(1986, 8, 1)
+                    }
+                },
+                Dependents = null
+            };
+
+            var printedPolicy = printer.PrintObject(policy);
+
+            Console.ReadKey();
         }
     }
 }
